e88ac47113f96d565efa0144779bed9876c184dd
Alina <alina.balgradean@gmail.com>
2018-09-02 18:26:10 +0200

    simply linked list

---
+++ simply_linked_list.py
@@ -0,0 +1,37 @@
+# Singly linked lists
+class HeadLinkedList:
+	def __init__(self):
+		self.headval = None
+
+	def traverse(self):
+		node = self.headval  
+		while node is not None:
+			print(node.val)
+			node = node.next 
+
+
+class Node:
+	def __init__(self, val):
+		self.val = val 
+		self.next = None 
+
+# Create the head of the linked list
+linkedlist = HeadLinkedList()
+# Create the head node of the linked list
+linkedlist.headval = Node('Monday')
+# Create the other nodes of the linked list
+z2 = Node('Tuesday')
+z3 = Node('Wednesday')
+z4 = Node('Thursday')
+z5 = Node('Friday')
+z6 = Node('Saturday')
+z7 = Node('Sunday')
+# Link the nodes together
+linkedlist.headval.next = z2 
+z2.next = z3 
+z3.next = z4 
+z4.next = z5
+z5.next = z6 
+z6.next = z7 
+
+linkedlist.traverse()
