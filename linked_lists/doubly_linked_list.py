2fdb516d2c0aa3ce60c267ca2d31aaae1cf5dc84
alinabalgradean <32243667+alinabalgradean@users.noreply.github.com>
2018-09-05 20:55:29 +0200

    Create doubly_linked_list.py

---
+++ linked_lists/doubly_linked_list.py
@@ -0,0 +1,35 @@
+# Doubly linked lists
+class HeadLinkedList:
+    def __init__(self):
+        self.headval = None
+
+class Node:
+    def __init__(self, val):
+        self.val = val 
+        self.previous = None
+        self.next = None 
+ 
+
+# Create the head of the linked list
+linkedlist = HeadLinkedList()
+# Create the head node of the linked list
+linkedlist.headval = Node('Monday')
+# Create the other nodes of the linked list
+z2 = Node('Tuesday')
+z3 = Node('Wednesday')
+z4 = Node('Thursday')
+z5 = Node('Friday')
+z6 = Node('Saturday')
+z7 = Node('Sunday')
+# Link the nodes together
+linkedlist.headval.next = z2 
+z2.previous = linkedlist
+z2.next = z3 
+z3.previous = z2
+z3.next = z4
+z4.previous = z3
+z4.next = z5
+z5.previous = z4
+z5.next = z6 
+z6.previous = z5
+z6.next = z7 
